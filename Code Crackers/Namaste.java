24f16d8814443549cb90924c43096385c1c359d7
Vinay Noah <vinaynoah@gmail.com>
2015-06-18 10:10:29 +0530

    few examples

---
+++ Code Crackers/Namaste.java
@@ -0,0 +1,40 @@
+
+class Speed {
+
+	public static void main(String[] felight){
+		double time = 1000;
+		double distance = 60600606;
+		
+		double speed = distance / time;
+		System.out.println(speed);
+	}
+
+}
+class Area {
+
+	public static void main(String[] felight) {
+		final double PI = 3.142;
+		double radius = 25.234;
+		double area =  PI * radius * radius;
+		System.out.println(area);
+		
+	}
+}
+
+class Add {
+
+	public static void main(String[] felight){
+		int num1 = 100;
+		int num2 = 10;
+		
+		int sum = num1 + num2;
+		
+		System.out.println("The addition of " + num1 + " & " + num2 + " is " + sum);
+	}
+
+}
+
+class Z {
+
+
+}
---
+++ Code Crackers/Test.java
@@ -0,0 +1,15 @@
+class Test {
+
+	public static void main(String[] felight){
+		
+		int age = 27;
+		//1.5
+		
+		System.out.printf(" %d is the vlue of age",age);
+		
+		
+	
+	
+	}
+
+}
